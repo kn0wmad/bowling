ad33870e6b57a6e068c934ee6684d5aa86a327e6
kn0wmad <kn0wmad@protonmail.com>
2025-05-07 20:19:36 -0600

    Refactor WIP - basic printing

--- frame.cpp
+++ frame.cpp
@@ -1,9 +1,14 @@
 #include "frame.h"
+#include <string>
 
 // Method definitions
 
 // Calculate score for each frame
 std::string Frame::Score(int roll1, int roll2) {
     std::string frameScore;
+
+    // TODO bonus logic
+
+    frameScore = std::to_string(roll1 + roll2);
     return frameScore;
 }
--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 */
 
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include "frame.h"
 
 // Constants
-const int TOTAL_FRAMES = 10;
-const int MAX_ROLLS = 21;
+const int TOTAL_FRAMES = 11;
 
 int main() {
     // Get file from user
@@ -25,15 +25,32 @@ int main() {
         std::cout << "Error reading file: " << bowlerFile << std::endl;
     }
 
-    // Create an array of Frame objects for the full game
+    // Create an array of Frame objects
     Frame gameFrames[TOTAL_FRAMES];
 
     // Iterate through each frame, giving array of objects individual states
-    for (int i = 0; i < TOTAL_FRAMES; ++i) {
+    for (int i = 0; i < TOTAL_FRAMES; i++) {
 
         // Set rolls in each frame
-        file >> gameFrames[i].roll1;                 // Input first roll from file and assign to new var for reuse
+        file >> gameFrames[i].roll1;                // Input first roll from file and assign to object's roll1
+        if (gameFrames[i].roll1 != 10) {
+            file >> gameFrames[i].roll2;            // Input second roll from file and assign to object's roll2
+        }
 
+        std::cout << "Frame |" << " |" << std::endl;
+        std::cout << "Throw |" << " |" << std::endl;
+        std::cout << "Score |" << " |" << std::endl;
+
+        for (int j = 0; j < i; j++) {
+            std::cout << std::setw(2) << i + 1 << std::setw(2);
+            if (gameFrames[j].roll1 != 10) {
+                std::cout << gameFrames[j].roll1 << gameFrames[j].roll2;
+                std::cout << gameFrames[j].Score(gameFrames[j].roll1, gameFrames[j].roll2) << " | ";
+            } else {
+                std::cout << gameFrames[j].roll1 << " | ";
+                std::cout << " X | ";
+            }
+        }
     }
 
     // Close the filestream
